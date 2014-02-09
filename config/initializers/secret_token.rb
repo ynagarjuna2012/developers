a708d890619d391c8e9e94a7e1cd8e233fa0df3f
ynagarjuna1995 <ynagarjuna2012@gmail.com>
2014-02-09 16:52:45 +0530

    before maknig blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArcmagnusDevelopers::Application.config.secret_key_base = '013123e50693cb55f3402b43e1190b5b6588c0f23786fdb2250afb8cb91e62314ed7f9c4392f063ad3e51b12090266b97311f70e28709ec0419324745f39b348'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArcmagnusDevelopers::Application.config.session_store :cookie_store, key: '_arcmagnusDevelopers_session'
